52d0ec6a2470c0923443ed2f95a9303f18e72a92
Mike Kazantsev <mk.fraggod@gmail.com>
2012-10-01 02:30:36 +0600

    Initial project boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+txSkyDrive
+----------------------------------------
+
+Twisted-based python async interface for [SkyDrive API (version
+5.0)](http://msdn.microsoft.com/en-us/library/live/hh826521).
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+pkg_root = os.path.dirname(__file__)
+
+# Error-handling here is to allow package to be built w/o README included
+try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
+except IOError: readme = ''
+
+setup(
+
+	name = 'txSkyDrive', # just trying to mimic tx* naming convention
+	version = '12.10.0',
+	author = 'Mike Kazantsev',
+	author_email = 'mk.fraggod@gmail.com',
+	license = 'WTFPL',
+	keywords = 'skydrive twisted api oauth2 rest microsoft cloud live liveconnect',
+	url = 'http://github.com/mk-fg/txskydrive',
+
+	description = 'Twisted-based async interface'
+		' for Microsoft LiveConnect SkyDrive REST API v5.0',
+	long_description = readme,
+
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Plugins',
+		'Framework :: Twisted',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'License :: OSI Approved',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2 :: Only',
+		'Topic :: Internet',
+		'Topic :: System :: Archiving',
+		'Topic :: System :: Filesystems' ],
+
+	install_requires = ['Twisted'],
+
+	packages = find_packages(),
+	include_package_data = True,
+	package_data = {'': ['README.txt']},
+	exclude_package_data = {'': ['README.*']} )
